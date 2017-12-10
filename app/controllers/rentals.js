64db55873f1c3815f4bfff4a637177e3475d414f
Timo Wang <chiuanw@yahoo-inc.com>
2017-12-10 09:50:25 +0800

    add list-filter-test

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -4,9 +4,17 @@ export default Controller.extend({
     actions: {
         filterByCity(param) {
             if (param !== '') {
-                return this.get('store').query('rental', { city: param });
+                return this.get('store')
+                    .query('rental', { city: param })
+                    .then((results) => {
+                        return { query: param, results: results };
+                    });
             }
-            return this.get('store').findAll('rental');
+            return this.get('store')
+                .findAll('rental')
+                .then((results) => {
+                    return { query: param, results: results };
+                });
         }
     }
 });
