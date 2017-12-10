08f69dcdfca1ad419354adc67b3cafab08aba6ef
Timo Wang <chiuanw@yahoo-inc.com>
2017-12-10 17:02:24 +0800

    add list-rentals-test acceptance test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,39 +3,12 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('should show rentals as the home page', function (assert) {
+test('should filter the list of rentals by city.', function (assert) {
     visit('/');
-    
-    andThen(function() {
-        assert.equal(currentURL(), '/rentals', 'should redirect automatically');
-    });
-});
-
-test('should link to information about the company.', function (assert) {
-    visit('/');
-    click('a:contains("About")');
+    fillIn('.list-filter input', 'Seattle');
+    keyEvent('.list-filter input', 'keyup', 69);
     andThen(function () {
-        assert.equal(currentURL(), '/about', 'should navigate to about');
+        assert.equal(find('.listing').length, 1, 'should show 1 listing');
+        assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
     });
 });
-
-test('should link to contact information.', function (assert) {
-    visit('/');
-    click('a:contains("Contact")');
-    andThen(function() {
-        assert.equal(currentURL(), '/contact', 'should navigate to contact');
-    });
-});
-
-test('should list available rentals.', function (assert) {
-    visit('/');
-    andThen(function () {
-        assert.equal(find('.listing').length, 3, 'should see 3 listings');
-    });
-});
-
-// test('should filter the list of rentals by city.', function (assert) {
-// });
-
-// test('should show details for a selected rental', function (assert) {
-// });
